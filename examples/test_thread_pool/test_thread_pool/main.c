7f762efe5c79adc96138ac2e3193f4b12f0e7dcc
whiledu <whiledu@gmail.com>
2012-07-03 16:20:43 +0800

    +添加测试线程池等待接口的代码

--- examples/test_thread_pool/test_thread_pool/main.c
+++ examples/test_thread_pool/test_thread_pool/main.c
@@ -36,25 +36,58 @@ printf("############### ThreadID : %d\n", (int)pthread_self());
 
 static void my_msg_fun(struct thread_pool_s* self, void* msg)
 {
+    thread_sleep(2000);
     showcounter();
 }
 
 int main()
 {
     int i= 0;
-    struct thread_pool_s* p = thread_pool_new(my_msg_fun, 3, 4000);
+    struct thread_pool_s* p = thread_pool_new(my_msg_fun, 4, 4000);
     count_mutex = mutex_new();
     thread_pool_start(p);
     
     thread_sleep(1000);
+    thread_pool_pushmsg(p, (void*)3);
+    thread_pool_pushmsg(p, (void*)3);
+    thread_pool_pushmsg(p, (void*)3);
+
+    thread_pool_pushmsg(p, (void*)3);
+    thread_pool_pushmsg(p, (void*)3);
+    thread_pool_pushmsg(p, (void*)3);
+    thread_pool_pushmsg(p, (void*)3);
+    thread_pool_pushmsg(p, (void*)3);
+    thread_pool_pushmsg(p, (void*)3);
+    thread_pool_pushmsg(p, (void*)3);
+    thread_pool_pushmsg(p, (void*)3);
+
+    thread_pool_wait(p);
+
+    printf("线程池工作完毕\n");
+
+    thread_pool_wait(p);
+
+    printf("线程池工作完毕\n");
+
+    thread_pool_pushmsg(p, (void*)3);
+    thread_pool_wait(p);
+
+    printf("线程池工作完毕\n");
+    getchar();
 
     while(1)
     {
         if(i < 3001)
         {
-            thread_pool_pushmsg(p, (void*)3);
+            //thread_pool_pushmsg(p, (void*)3);
             i++;
         }
+        else
+        {
+            //break;
+        }
     }
+
+    thread_pool_delete(p);
     return 0;
 }
